d3e9d25b039bed0df43b30a0ab4b8a1ef6c5dec0
positiveshulin <shulin_wang@qq.com>
2018-10-23 23:38:27 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/swufe/reading_records/BookshelfActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.swufe.reading_records;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookshelfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookshelf);
+    }
+}
---
+++ app/src/main/java/cn/edu/swufe/reading_records/RecordsActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.swufe.reading_records;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_records);
+    }
+}
